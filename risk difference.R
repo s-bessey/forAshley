ced40e6a4a8478011124abb693af76a7c46f8407
sarah_bessey@brown.edu <sarah_bessey@brown.edu>
2018-12-10 13:49:03 -0500

    defined function of disseminated, overall, and composite differences and ratios for each individual treatment

--- risk difference.R
+++ risk difference.R
@@ -11,11 +11,37 @@ contourValuesCumulative <- contourValuesDisseminated
 rownames(contourValues) <- matrixRowNames
 colnames(contourValues) <- matrixRowNames
 
-RD10 <- riskdifference(aveTreatment10$HIV_PrEP,aveTreatment10$HIV_PrEP,
-                       aveTreatment10$PrEP, aveTreatment10$noPrEP)
+#risk ratios and risk differences for disseminated
 
-RD10 <- riskratio(aveTreatment10$HIV_PrEP,aveTreatment10$HIV_PrEP,
-                       aveTreatment10$PrEP, aveTreatment10$noPrEP)
+riskDiffRatio <- function(treatment){
+  
+  naming <- substr(treatment, nchar(treatment)-2,nchar(treatment))
+  dissDiff <- riskdifference(treatment$HIV_noPrEP,treatment$HIV_control,
+                             treatment$noPrEP,treatment$control)
+  dissRatio <- riskratio(treatment$HIV_noPrEP,treatment$HIV_control,
+                         treatment$noPrEP,treatment$control)
+  assign(print("disseminatedDiff",naming, sep=""),dissDiff)
+  assign(print("disseminatedRatio",naming, sep=""),dissRatio)
+  
+  overallDiff <- riskdifference(treatment$HIV,treatment$HIV_control,
+                                  (treatment$PrEP+treatment$noPrEP),treatment$control)
+  overallRatio <- riskratio(treatment$HIV,treatment$HIV_control,
+                              (treatment$PrEP+treatment$noPrEP),treatment$control)
+  assign(print("overallDiff",naming, sep=""),overallDiff)
+  assign(print("overallRatio",naming, sep=""),overallRatio)
+  
+  compositeDiff <- riskdifference(treatment$HIV_PrEP,treatment$HIV_control,
+                                treatment$PrEP,treatment$control)
+  compositeRatio <- riskratio(treatment$HIV_PrEP,treatment$HIV_control,
+                                  treatment$PrEP,treatment$control)
+  assign(print("compositeDiff",naming, sep=""),compositeDiff)
+  assign(print("overallRatio",naming, sep=""),compositeRatio)
+  
+}
+
+
+
+
 #need to write total person-years on/off PrEP and the number w/HIV for both
 
 #start with disseminated, then do composite
@@ -25,14 +51,14 @@ RD10 <- riskratio(aveTreatment10$HIV_PrEP,aveTreatment10$HIV_PrEP,
 #put risk difference at matrix["a","b"]
 #put negative of risk difference at matrix["b","a"]
 
-incifferenceAndRatio <- function(treatment1,treatment2,mtx){ #make this work for disseminated and composite
+differenceAndRatio <- function(treatment1,treatment2,mtx){ #make this work for disseminated and composite
   avg1 <- aggregate(.~t, treatment1,function(x) mean = mean(x))
   disseminatedDiff <- riskdifference(treatment1$HIV_noPrEP,treatment2$HIV_noPrEP,
                                      treatment1$noPrEP,treatment2$noPrEP)
   xLocation <- substr(treatment1, nchar(treatment1)-2, nchar(treatment1)-1)
-  xLocation <- paste(".",xLocation)
+  xLocation <- paste(".",xLocation) #may not need this if coverage is decimal?
   yLocation <- substr(treatment2, nchar(treatment2)-2, nchar(treatment2))
-  yLocation <- paste(".",yLocation)
+  yLocation <- paste(".",yLocation) #see above
   mtx[xLocation,yLocation] <- disseminatedDiff
   #will this work??
 }
