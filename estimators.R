4bf911d01516d035ad1492d8dae65cb1f48599c7
sarah_bessey@brown.edu <sarah_bessey@brown.edu>
2018-12-21 16:44:52 -0500

    Fixed quantiles, attempted to fix estimators

--- estimators.R
+++ estimators.R
@@ -3,7 +3,7 @@
 # of changing as necessary
 N <- 11245
 setwd("Ashley_runs")
-temp=list.files(pattern = "*10*")
+temp=list.files(pattern = "*10_*")
 # This should be a function, but it was easier to do this way
 # these read the names of everything for a given coverage level, then bind those
 # into one data frame
@@ -26,10 +26,11 @@ for (i in 1:length(temp)){
 treatment20 <-do.call(rbind,datalist)
 
 
-temp=list.files(pattern = "*30_*")
+temp=list.files(pattern = "*30*")
 for (i in 1:length(temp)){
   assign(paste("temp2",sep = ""), read.table(temp[i],header = TRUE))
   #assign(temp[i],temp2)
+  datalist <- NULL
   datalist[[i]] <- temp2
 }
 treatment30 <-do.call(rbind,datalist)
@@ -93,9 +94,6 @@ treatment90 <-do.call(rbind,datalist)
 
 
 #this calculates the estimators
-
-
-
 estimators <- function(treatment,coverage){
   # adds a column of zeroes (non-treatment) or ones (treatment)
   # should have used an if statement, can be fixed later
@@ -109,27 +107,34 @@ estimators <- function(treatment,coverage){
   infectionValues <- as.data.frame(cbind(time60$nseed,time60$compID))
   colnames(infectionValues) <- c("nseed","compID")
   
-  #
+  # start to append needed values for calculations to the data frame
+  # find total people in each component (t = 0)
   infectionValues$totalN <- time0$totalN
-  #end_01 <- subset(time60, time0$NprepElig != 0)
-  infectionValues$N_01 <- time0$NprepElig +time0$Nhiv*time0$treatmentgp
-  infectionValues$HIV_01 <- time0$NprepElig - time60$NprepElig + time0$Nhiv*time0$treatmentgp # no. of people who
-          # lose prep elig = no. prep elig infected (plus those who started HIV+)
-
+  # number of non-treated people in treatment group at time 0
+  # includes PrEP-eligible and those who start infected
+  infectionValues$N_01 <- time0$NprepElig +(time0$Nhiv*time0$treatmentgp)
+  # people lose PrEP eligibility by becoming infected. Other infections are
+  # those who started the model on PrEP. So # who stop being eligible + number
+  # who started HIV+ = prevalence among untreated treatment group
+  infectionValues$HIV_01 <- (time0$NprepElig - time60$NprepElig) + (time0$Nhiv*time0$treatmentgp)
+  # see above
   infectionValues$Inc_01 <- time0$NprepElig - time60$NprepElig # no. of people who
   # lose prep elig = no. prep elig infected
   infectionValues$N_11 <- time0$Nprep # number of people on PrEP
-  infectionValues$HIV_11 <- time60$Nhiv * time60$treatmentgp - infectionValues$HIV_01
-  infectionValues$Inc_11 <- time60$Nnewinf * time60$treatmentgp - infectionValues$Inc_01
-  
+  # see above re: treatment group infections
+  infectionValues$HIV_11 <- 0
+  infectionValues$Inc_11 <- (time60$Nnewinf * time0$treatmentgp) - infectionValues$Inc_01
+
   infectionValues$N_trt_group <- infectionValues$N_11 + infectionValues$N_01
   
-  #N_00 <- time0$N[treatment$NprepElig == 0 & treatment$Nprep == 0 & treatment$t == 0] # population of control gp
-  infectionValues$HIV_00 <- time60$Nhiv - infectionValues$HIV_11 - infectionValues$HIV_01 # HIV population of control gp
-  infectionValues$Inc_00 <- time60$Nnewinf - infectionValues$Inc_11 - infectionValues$Inc_01
-  
-  #individual weights
+  # infectionValues$N_00 <- time0$totalN * (1- time0$treatmentgp) # population of control gp
+  infectionValues$HIV_00 <- time60$Nhiv - infectionValues$HIV_01- infectionValues$HIV_11 # HIV population of control gp
+  infectionValues$Inc_00 <- time60$Nnewinf * (1- time0$treatmentgp)
+  assign("HIV_11_check", infectionValues$Inc_00, envir = .GlobalEnv)
 
+  #individual weights
+  # number of components (length of each nseed)
+  # the average just makes it populate to everything in same nseed
   K <- with(time0,ave(compID,nseed,FUN = length))
   
   infectionValues$w_h <-  1/(infectionValues$totalN*K)
@@ -138,12 +143,16 @@ estimators <- function(treatment,coverage){
 
 
   #find sums for each group
-  weights <- as.data.frame(cbind(infectionValues$nseed,infectionValues$N_trt_group, infectionValues$N_11*infectionValues$N_trt_group,
+  #fist define each equation that's summed in the weight calcs
+  weights <- as.data.frame(cbind(infectionValues$nseed,infectionValues$N_trt_group,
+                                 infectionValues$N_11*infectionValues$N_trt_group,
                               (1-infectionValues$N_11)*infectionValues$N_trt_group,
                               1-infectionValues$N_trt_group))
  
   colnames(weights) <- c("nseed", "sum1", "sum2","sum3","sum4")
+  # take the sums needed
   sums1 <- aggregate(.~nseed,weights, function(x) sum = sum(x))
+  # repopulate with a sums column the same length as the df
   sums <- merge(sums1, infectionValues, by = "nseed")
   
   #infectionValues$sum1 <- tapply(infectionValues$N_trt_group,INDEX = infectionValues$nseed, sum)
@@ -151,47 +160,56 @@ estimators <- function(treatment,coverage){
 
 
   
-  #two stage inverse probability weights
+  # two stage inverse probability weights
+  # use sums from above
   w11 <- (sums$sum1/N)^(-1) *(sums$sum2/sums$sum1)^(-1)
   assign("test", w11,envir = .GlobalEnv)
 
   w01 <- (sums$sum1/N)^(-1) * (sums$sum3/sums$sum1)^(-1)
   w0 <- (sums$sum4/N)^(-1)
   w1 <- (sums$sum1/N)^(-1)
+  assign("wcheck", w1, envir = .GlobalEnv)
+  Y_prev <- infectionValues$HIV_11 * infectionValues$N_11 * infectionValues$N_trt_group +
+    infectionValues$HIV_01 * (1-infectionValues$N_11) *infectionValues$N_trt_group +
+    infectionValues$HIV_00 * (1-infectionValues$N_11) * (1-infectionValues$N_trt_group)
+  
+  Y_inc <- infectionValues$Inc_11 * infectionValues$N_11 * infectionValues$N_trt_group +
+    infectionValues$Inc_01 * (1-infectionValues$N_11) *infectionValues$N_trt_group +
+    infectionValues$Inc_00 * (1-infectionValues$N_11) * (1-infectionValues$N_trt_group)
 
 
   #risks
-  #risk11_prev <- tapply(w11*w_h*infectionValues$HIV_11*infectionValues$N_11*infectionValues$N_trt_group, infectionValues$nseed, sum)
+  # this calculates them separately then finds the sum
   risk11_prev <- w11*infectionValues$w_h*infectionValues$HIV_11*infectionValues$N_11*infectionValues$N_trt_group
   risk11_prev <- tapply(risk11_prev, infectionValues$nseed, sum)
   
-  risk01_prev <- w01*w_h*infectionValues$HIV_01*(1-infectionValues$N_11)
+  risk01_prev <- w01*infectionValues$w_h*infectionValues$HIV_01*(1-infectionValues$N_11)
   risk01_prev <- tapply(risk01_prev, infectionValues$nseed, sum)
   
-  risk00_prev <- w0*w_h*infectionValues$HIV_00*(1-infectionValues$N_11)*(1 - infectionValues$N_trt_group)
+  risk00_prev <- w0*infectionValues$w_h*infectionValues$HIV_00 #*(1-infectionValues$N_11)*(1 - infectionValues$N_trt_group)
   risk00_prev <- tapply(risk00_prev, infectionValues$nseed, sum)
   # 
-  risk1_prev <- w1 * w_h * (infectionValues$HIV_11 + infectionValues$HIV_01) * infectionValues$N_trt_group
+  risk1_prev <- w1 * infectionValues$w_h * (infectionValues$HIV_11 + infectionValues$HIV_01) * infectionValues$N_trt_group
   risk1_prev <- tapply(risk1_prev, infectionValues$nseed, sum)
   risk0_prev <- risk00_prev
   #incidence risk
   risk11_inc <- w11*infectionValues$w_h*infectionValues$Inc_11*infectionValues$N_11*infectionValues$N_trt_group
   risk11_inc <- tapply(risk11_inc, infectionValues$nseed, sum)
   
-  risk01_inc <- w01*w_h*infectionValues$Inc_01*(1-infectionValues$N_11)
+  risk01_inc <- w01*infectionValues$w_h*infectionValues$Inc_01*(1-infectionValues$N_11)
   risk01_inc <- tapply(risk01_inc, infectionValues$nseed, sum)
   
-  risk00_inc <- w0*w_h*infectionValues$Inc_00*(1-infectionValues$N_11)*(1 - infectionValues$N_trt_group)
+  risk00_inc <- w0*infectionValues$w_h*infectionValues$Inc_00 #*(1-infectionValues$N_11)*(1 - infectionValues$N_trt_group)
   risk00_inc <- tapply(risk00_inc, infectionValues$nseed, sum)
   # 
-  risk1_inc <- w1 * w_h * (infectionValues$Inc_11 + infectionValues$Inc_01) * infectionValues$N_trt_group
+  risk1_inc <- w1 * infectionValues$w_h * (infectionValues$Inc_11 + infectionValues$Inc_01) * infectionValues$N_trt_group
   risk1_inc <- tapply(risk1_inc, infectionValues$nseed, sum)
-  risk0_inc <- risk00inc
+  risk0_inc <- risk00_inc
   
 
-  risk <- cbind(risk11_prev,risk01_prev,risk00_prev,risk1_prev,risk0_prev, risk11_inc,risk01_inc,risk00_inc,risk1_inc,risk0_inc)
+  risk <- as.data.frame(cbind(risk11_prev,risk01_prev,risk00_prev,risk1_prev,risk0_prev, risk11_inc,risk01_inc,risk00_inc,risk1_inc,risk0_inc))
   title <- paste("risk", coverage, sep = "")
   
-  assign(title,risk, envir = .GlobalEnv)
+  assign(title, risk, envir = .GlobalEnv)
 }
 
--- singleCoverageRisk.R
+++ singleCoverageRisk.R
@@ -3,7 +3,7 @@
 ## notes: need to make separate function for all estimators (include quantiles)
 ## then: through this, do risk calculations
 # also need to add all to separate table for plotting?
-
+library(sjPlot)
 #have to have cum and prev ave first
 table2 <- as.data.frame(matrix(nrow = 4, ncol = 5))
 colnames(table2) <- c('Effect', 'RD', '95% CI','RR','95% CI')
@@ -17,87 +17,77 @@ riskDiffRatio <- function(riskDF,tbl1,tbl2,coverage){ #give the file of endtime 
   naming2 <- paste(coverage,"Prev", sep = "") # name for prevalence
   naming4 <- paste(coverage,"Inc", sep = "") # name for incidence
   
-  #quantiles
-  # get per run risks then quantiles
-  # Prev_11_quants <- quantile(riskDF$risk11_prev,probs = c(.025,.975),na.rm = T)
-  # Prev_01_quants <- quantile(riskDF$risk01_prev,probs = c(.025,.975),na.rm = T)
-  # Prev_00_quants <- quantile(riskDF$risk00_prev,probs = c(.025,.975),na.rm = T)
-  # Prev_1_quants <- quantile(riskDF$risk1_prev,probs = c(.025,.975),na.rm = T)
-  # 
-  # Inc_11_quants <- quantile(riskDF$risk11_inc,probs = c(.025,.975),na.rm = T)
-  # Inc_01_quants <- quantile(riskDF$risk01_inc,probs = c(.025,.975),na.rm = T)
-  # Inc_00_quants <- quantile(riskDF$risk00_inc,probs = c(.025,.975),na.rm = T)
-  # Inc_1_quants <- quantile(riskDF$risk1_inc,probs = c(.025,.975),na.rm = T)
-
-  # create differences and ratios for both prevalence and incidence
+# -----
   # individual prevalence
   #individualDiff <- mean(riskDF$risk11) - mean(riskDF$risk01)
   individualDiff <- riskDF$risk11_prev - riskDF$risk01_prev
-  tbl1[1,2] <- ave(individualDiff) #add to table
-
+  tbl1[1,2] <-mean(individualDiff) #add to table
 
   #lowerPrevCI
-  lowerPrevCI<-quantile(individualDiff, probs = c(.025, .975))
-  upperPrevCI <- Prev_11_quants[2]-Prev_01_quants[1]
+  PrevCI<-quantile(individualDiff, probs = c(.025, .975))
+  upperPrevCI <- PrevCI[2]
+  lowerPrevCI <- PrevCI[1]
   tbl1[1,3] <- paste(lowerPrevCI,upperPrevCI) #find simulation intervals (2.5 and 97.5 percentiles)
-  assign(paste("individualDiff",naming2,sep=""),c(individualDiff,lowerPrevCI,upperPrevCI), envir = .GlobalEnv)
-  individualRatio <- mean(riskDF$risk11_prev)/mean(riskDF$risk01_prev)
-  tbl1[1,4] <- individualRatio
-  lowerCI <- Prev_11_quants[1]/Prev_01_quants[2]
-  upperCI <- Prev_11_quants[2]/Prev_01_quants[1]
+  assign(paste("individualDiff",naming2,sep=""),c(mean(individualDiff),lowerPrevCI,upperPrevCI), envir = .GlobalEnv)
+  individualRatio <- riskDF$risk11_prev/riskDF$risk01_prev
+  tbl1[1,4] <- mean(individualRatio)
+  CI<-quantile(individualRatio, probs = c(.025, .975))
+  lowerCI <- CI[1]
+  upperCI <- CI[2]
   tbl1[1,5] <- paste(lowerCI,upperCI)
   
-  assign(paste("individualRatio",naming2,sep=""),individualRatio)
+  #assign(paste("individualRatio",naming2,sep=""),mean(individualRatio), lowerCI, upperCI)
 
 
   # incidence individual
-  indIncDiff <- mean(riskDF$risk11_inc)-mean(riskDF$risk01_inc)
-  tbl2[1,2] <- indIncDiff # risk difference for incidence
+  indIncDiff <-riskDF$risk11_inc - riskDF$risk01_inc
+  tbl2[1,2] <- mean(indIncDiff) # risk difference for incidence
   #confidence intervals
-  lowerIncCI <- Inc_11_quants[1]-Inc_01_quants[2]
-  upperIncCI <- Inc_11_quants[2]-Inc_01_quants[1]
+  lowerIncCI <- quantile(indIncDiff, probs = .025)
+  upperIncCI <- quantile(indIncDiff, probs = .975)
   tbl2[1,3] <- paste(lowerIncCI,upperIncCI)
-  assign(paste("fig3", coverage, sep = "_"), c(indIncDiff,lowerIncCI,upperIncCI), envir = .GlobalEnv)
-  indIncRatio <- mean(riskDF$risk11_inc)/mean(riskDF$risk01_inc)
-  lowerIncCI <- Inc_11_quants[1]/Inc_01_quants[2]
-  upperIncCI <- Inc_11_quants[2]/Inc_01_quants[1]
-  tbl2[1,4] <- indIncRatio
+  #assign(paste("individualDiff",naming4,sep=""),indIncDiff)
+  assign(paste("fig3", coverage, sep = "_"), c(mean(indIncDiff),lowerIncCI,upperIncCI), envir = .GlobalEnv)
+  indIncRatio <- riskDF$risk11_inc/riskDF$risk01_inc
+  lowerIncCI <- quantile(indIncRatio, probs = .025)
+  upperIncCI <- quantile(indIncRatio, probs = .975)
+  tbl2[1,4] <- mean(indIncRatio)
   tbl2[1,5] <- paste(lowerIncCI,upperIncCI)
-  assign(paste("individualDiff",naming4,sep=""),indIncDiff)
-  assign(paste("individualRatio",naming4,sep=""),indIncRatio)
+
+  #assign(paste("individualRatio",naming4,sep=""),c(mean(indIncRatio), lowerIncCI,upperIncCI), envir = .GlobalEnv)
   
   
   #-----
   #fix difference and ratios beyond this point
   # disseminated prevalence risk
-  dissDiff <- mean(riskDF$risk01_prev) - mean(riskDF$risk00_prev)
+  dissDiff <- riskDF$risk01_prev - riskDF$risk00_prev
 
-  tbl1[2,2] <- dissDiff
-  lowerCI <- Prev_01_quants[1] - Prev_00_quants[2]
-  upperCI <- Prev_01_quants[2] - Prev_00_quants[1]
+  tbl1[2,2] <- mean(dissDiff)
+  lowerCI <- quantile(dissDiff, probs = .025)
+  upperCI <- quantile(dissDiff, probs = .975)
   tbl1[2,3] <- paste(lowerCI,upperCI)
   
-  dissRatio <- mean(riskDF$risk01_prev) / mean(riskDF$risk00_prev)
-  tbl1[2,4] <- dissRatio
-  lowerCI <- Prev_01_quants[1] / Prev_00_quants[2]
-  upperCI <- Prev_01_quants[2] / Prev_00_quants[1]
+  dissRatio <- riskDF$risk01_prev / riskDF$risk00_prev
+  tbl1[2,4] <- mean(dissRatio)
+  lowerCI <- quantile(dissRatio, probs = .025)
+  upperCI <- quantile(dissRatio, probs = .975)
   tbl1[2,5] <- paste(lowerCI,upperCI)
   
   assign(paste("disseminatedDiff",naming2, sep=""),dissDiff)
   assign(paste("disseminatedRatio",naming2, sep=""),dissRatio)
   
   #disseminated incident risk
-  dissIncDiff <- mean(riskDF$risk01_inc) - mean(riskDF$risk00_inc)
-  tbl2[2,2] <- dissIncDiff
-  lowerCI <- Inc_01_quants[1] - Inc_00_quants[2]
-  upperCI <- Inc_01_quants[2] - Inc_00_quants[1]
+  dissIncDiff <- riskDF$risk01_inc - riskDF$risk00_inc
+  tbl2[2,2] <- mean(dissIncDiff)
+  lowerCI <- quantile(dissIncDiff, probs = .025)
+  upperCI <- quantile(dissIncDiff, probs = .975)
   tbl2[2,3] <- paste(lowerCI,upperCI)
   assign(paste("fig4", coverage, sep = "_"), c(dissIncDiff,lowerCI,upperCI), envir = .GlobalEnv)
   
-  dissIncRatio <- mean(riskDF$risk01_inc) / mean(riskDF$risk00_inc)
-  tbl2[2,4] <-dissIncRatio
-  lowerCI <- Inc_01_quants[1] / Inc_00_quants[2]
-  upperCI <- Inc_01_quants[2] / Inc_00_quants[1]
+  dissIncRatio <- riskDF$risk01_inc / riskDF$risk00_inc
+  tbl2[2,4] <- mean(dissIncRatio)
+  lowerCI <- quantile(dissIncRatio, probs = .025)
+  upperCI <- quantile(dissIncDiff, probs = .975)
   tbl2[2,5] <- paste(lowerCI,upperCI)
   
   assign(paste("disseminatedDiff",naming4, sep=""),dissIncDiff)
@@ -105,62 +95,63 @@ riskDiffRatio <- function(riskDF,tbl1,tbl2,coverage){ #give the file of endtime 
 
   # overall prevalence risk
   
-  overallDiff <- mean(riskDF$risk1_prev) - mean(riskDF$risk00_prev)
-  tbl1[4,2] <- overallDiff
-  lowerCI <- Prev_1_quants[1] - Prev_00_quants[2]
-  upperCI <- Prev_1_quants[2] - Prev_00_quants[1]
+  overallDiff <- riskDF$risk1_prev - riskDF$risk00_prev
+  tbl1[4,2] <- mean(overallDiff)
+  lowerCI <- quantile(overallDiff, probs = .025)
+  upperCI <- quantile(overallDiff, probs = .975)
   tbl1[4,3] <- paste(lowerCI,upperCI)
   
-  overallRatio <-  mean(riskDF$risk1_prev) / mean(riskDF$risk00_prev)
-  tbl1[4,4] <- overallRatio
-  lowerCI <- Prev_1_quants[1] / Prev_00_quants[2]
-  upperCI <- Prev_1_quants[2] / Prev_00_quants[1]
+  overallRatio <-  riskDF$risk1_prev / riskDF$risk00_prev
+  tbl1[4,4] <- mean(overallRatio)
+  lowerCI <- quantile(overallRatio, probs = .025)
+  upperCI <- quantile(overallRatio, probs = .975)
   tbl1[4,5] <- paste(lowerCI,upperCI)
   
   assign(paste("overallDiff",naming2, sep=""),overallDiff)
   assign(paste("overallRatio",naming2, sep=""),overallRatio)
   
   #overall incidence risk
-  overallIncDiff <-  mean(riskDF$risk1_inc) - mean(riskDF$risk00_inc)
-  tbl2[4,2] <- overallIncDiff
-  lowerCI <- Inc_1_quants[1] -Inc_00_quants[2]
-  upperCI <- Inc_1_quants[2] - Inc_00_quants[1]
+  overallIncDiff <-  riskDF$risk1_inc - riskDF$risk00_inc
+  tbl2[4,2] <- mean(overallIncDiff)
+  lowerCI <- quantile(overallIncDiff, probs = .025)
+  upperCI <- quantile(overallIncDiff, probs = .975)
   tbl2[4,3] <- paste(lowerCI,upperCI)
-  overallIncRatio <- mean(riskDF$risk1_inc) / mean(riskDF$risk00_inc)
-  tbl2[4,4] <- overallIncRatio
-  lowerCI <- Inc_1_quants[1] / Inc_00_quants[2]
-  upperCI <- Inc_1_quants[2] / Inc_00_quants[1]
+  overallIncRatio <- riskDF$risk1_inc / riskDF$risk00_inc
+  tbl2[4,4] <- mean(overallIncRatio)
+  lowerCI <- quantile(overallIncRatio, probs = .025)
+  upperCI <- quantile(overallIncRatio, probs = .975)
   tbl2[4,5] <- paste(lowerCI,upperCI)
   
   assign(paste("overallDiff",naming4, sep=""),overallIncDiff)
   assign(paste("overallRatio",naming4, sep=""),overallIncRatio)
   
   # composite prevalence risk
-  compositeDiff <- mean(riskDF$risk11_prev) - mean(riskDF$risk00_prev)
-  tbl1[3,2] <- compositeDiff
-  lowerCI <- Inc_11_quants[1] -Inc_00_quants[2]
-  upperCI <- Inc_11_quants[2] - Inc_00_quants[1]
+  compositeDiff <- riskDF$risk11_prev - riskDF$risk00_prev
+  tbl1[3,2] <- mean(compositeDiff)
+  lowerCI <- quantile(compositeDiff, probs = .025)
+  upperCI <- quantile(compositeDiff, probs = .975)
   tbl1[3,3] <- paste(lowerCI,upperCI)
   
-  compositeRatio <- mean(riskDF$risk11_prev) / mean(riskDF$risk00_prev)
-  tbl1[3,4] <- compositeRatio
-  lowerCI <- Prev_11_quants[1] /Prev_00_quants[2]
-  upperCI <- Prev_11_quants[2] / Prev_00_quants[1]
+  compositeRatio <- riskDF$risk11_prev / riskDF$risk00_prev
+  tbl1[3,4] <- mean(compositeRatio)
+  lowerCI <- quantile(compositeRatio, probs = .025)
+  upperCI <- quantile(compositeRatio, probs = .975)
   tbl1[3,5] <- paste(lowerCI, upperCI)
   
   assign(paste("compositeDiff",naming2, sep=""),compositeDiff)
   assign(paste("overallRatio",naming2, sep=""),compositeRatio)
   
   # composite incidence risk
-  compositeIncDiff <- mean(riskDF$risk11_inc) - mean(riskDF$risk00_inc)
-  tbl2[3,2] <- compositeIncDiff
-  lowerCI <- Inc_11_quants[1] -Inc_00_quants[2]
-  upperCI <- Inc_11_quants[2] - Inc_00_quants[1]
+  compositeIncDiff <- riskDF$risk11_inc - riskDF$risk00_inc
+  tbl2[3,2] <- mean(compositeIncDiff)
+  lowerCI <- quantile(compositeIncDiff, probs = .025)
+  upperCI <- quantile(compositeIncDiff, probs = .975)
   tbl2[3,3] <- paste(lowerCI,upperCI)
-  compositeIncRatio <- mean(riskDF$risk11_inc) / mean(riskDF$risk00_inc)
-  tbl2[3,4] <- compositeIncRatio
-  lowerCI <- Inc_11_quants[1] /Inc_00_quants[2]
-  upperCI <- Inc_11_quants[2] / Inc_00_quants[1]
+  
+  compositeIncRatio <- riskDF$risk11_inc / riskDF$risk00_inc
+  tbl2[3,4] <- mean(compositeIncRatio)
+  lowerCI <- quantile(compositeIncRatio, probs = .025)
+  upperCI <- quantile(compositeIncRatio, probs = .975)
   tbl2[3,5] <- paste(lowerCI,upperCI)
   assign(paste("compositeDiff",naming4, sep=""),compositeIncDiff)
   assign(paste("overallRatio",naming4, sep=""),compositeIncRatio)
@@ -168,12 +159,16 @@ riskDiffRatio <- function(riskDF,tbl1,tbl2,coverage){ #give the file of endtime 
   assign(paste("table4",coverage, sep="_"),tbl2, envir = .GlobalEnv)
 }
 
-riskDiffRatio(risk10,table2, table4,10) #treatment 10%
-
+riskDiffRatio(risk30,table2, table4,30) 
+riskDiffRatio(risk70,table2, table4,70) 
 
 #tables
-tab_df(table2_50, col.header = colnames(table2), show.rownames = F, alternate.rows = T,file="Table2.doc")
-tab_df(table4_50, col.header = colnames(table4), show.rownames = F, alternate.rows = T,file="Table4.doc")
+tab_df(table2_30, col.header = colnames(table2), show.rownames = F, alternate.rows = T,file="Table2_30.doc")
+tab_df(table4_30, col.header = colnames(table4), show.rownames = F, alternate.rows = T,file="Table4_30.doc")
+
+tab_df(table2_70, col.header = colnames(table2), show.rownames = F, alternate.rows = T,file="Table2_70.doc")
+tab_df(table4_70, col.header = colnames(table4), show.rownames = F, alternate.rows = T,file="Table4_70.doc")
+
 
 
 # figure 3 (incidence)
