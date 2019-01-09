f3a418181532797f3446a498c3b68c0fd97de2c4
sarah_bessey@brown.edu <sarah_bessey@brown.edu>
2019-01-09 16:18:20 -0500

    got rid of extraneous lines

--- risk difference.R
+++ risk difference.R
@@ -3,9 +3,6 @@ library("knitr")
 library("dplyr")
 #import files
 
-dfTreatment10 <- read.table() #insert filename
-
-aveTreatment10 <- aggregate(.~t, dfTreatment10,function(x) mean = mean(x))
 
 
 contourValuesDisseminated <- matrix(nrow=9,ncol=9)
@@ -36,25 +33,25 @@ tab_df(df,title = "Contours", col.header = matrixRowNames, show.rownames = T, al
 #put negative of risk difference at matrix["b","a"]
 
 differenceAndRatioContours <- function(risk1,risk2,mtx1,mtx2,mtx3){ #make this work for disseminated and composite
-  #avg1 <- aggregate(.~t, risk1,function(x) mean = mean(x))
+
   disseminatedDiff <- mean(risk1$risk01_inc) - mean(risk2$risk00_inc)
 
   compositeDiff <- mean(risk1$risk11_inc) - mean(risk2$risk00_inc)
   
   overallDiff <- mean(risk1$risk1_inc) - mean(risk2$risk00_inc)
   xLocation <- substr(risk1, nchar(risk1)-2, nchar(risk1)-1)
-  xLocation <- paste(".",xLocation) #may not need this if coverage is decimal?
+  #xLocation <- paste(".",xLocation) #may not need this if coverage is decimal?
   yLocation <- substr(risk2, nchar(risk2)-2, nchar(risk2))
-  yLocation <- paste(".",yLocation) #see above
+  #yLocation <- paste(".",yLocation) #see above
   mtx1[xLocation,yLocation] <- disseminatedDiff
   #mtx1[yLocation,xLocation] <- -disseminatedDiff
-  mtx2[xLocation,yLocation] <- compositeDiff
+  #mtx2[xLocation,yLocation] <- compositeDiff
   #mtx2[ylocation,xLocation] <- -compositeDiff
   #will this work??
 }
 for (i in 1:9){
   for (j in 1:9){
-    differenceAndRatioContours(paste("risk",i,"0", sep = ""),paste("risk",j,"0", sep = ""),
+    differenceAndRatioContours(paste("riskDF_",i,"0", sep = ""),paste("riskDF_",j,"0", sep = ""),
                                contourValuesDisseminated, contourValuesCumulative,
                                contourValuesOverall)
   }
