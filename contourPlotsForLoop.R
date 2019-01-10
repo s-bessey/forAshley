c8e3a349154fac232220bf5033d136a134ab3d6f
sarah_bessey@brown.edu <sarah_bessey@brown.edu>
2019-01-10 14:18:26 -0500

    deleted uneccesary code, added overall and composite differences

--- contourPlotsForLoop.R
+++ contourPlotsForLoop.R
@@ -7,10 +7,16 @@ differenceAndRatioContoursTest <- function(mtx1,mtx2,mtx3){ #make this work for 
 
     for (j in 1:9){
       assign("r11", get(as.character(riskNames[i])))
+      assign("r21", get(as.character(riskNames[j])))
       #r21 <- get(riskNames[j])
       #differenceAndRatioContours(r11,r21,contourValuesDisseminated, contourValuesComposite,
       #                           contourValuesOverall)
       disseminatedDiff <- mean(r11$risk01_inc) - mean(r21$risk01_inc)
+      
+      compositeDiff <- mean(r11$risk11_inc) - mean(r21$risk01_inc)
+      
+      overallDiff <- mean(r11$risk1_inc) - mean(r21$risk1_inc)
+      
       name1 <- riskNames[i]
       name2 <- riskNames[j]
       xLocation <- as.numeric(substr(name1, nchar(name1)-1, nchar(name1)-1))
@@ -18,29 +24,21 @@ differenceAndRatioContoursTest <- function(mtx1,mtx2,mtx3){ #make this work for 
       
       mtx1[xLocation,yLocation] <- disseminatedDiff
       mtx1[yLocation,xLocation] <- -disseminatedDiff
+      mtx2[xLocation,yLocation] <- compositeDiff
+      mtx2[yLocation,xLocation] <- -compositeDiff
+      mtx3[xLocation,yLocation] <- overallDiff
+      mtx3[yLocation,xLocation] <- -overallDiff
       assign("contourValuesDisseminated", value = mtx1, envir = .GlobalEnv)
+      assign("contourValuesComposite", value = mtx2, envir = .GlobalEnv)
+      assign("contourValuesOverall", value = mtx3, envir = .GlobalEnv)
     }
       
   }
-  
-  
-return(name1)
-  #disseminatedDiff <- mean(risk1$risk01_inc) - mean(risk2$risk01_inc)
-  
-  xLocation <- as.numeric(substr(name1, nchar(name1)-1, nchar(name1)-1))
-  yLocation <- as.numeric(substr(name2, nchar(name2)-1, nchar(name2)-1))
-  
-  mtx1[xLocation,yLocation] <- disseminatedDiff
-  mtx1[yLocation,xLocation] <- -disseminatedDiff
-  
-  #mtx2[xLocation,yLocation] <- compositeDiff
-  #mtx2[yLocation,xLocation] <- -compositeDiff
-  #mtx3[xLocation,yLocation] <- overallDiff
-  #mtx3[yLocation,xLocation] <- -overallDiff
-  #will this work??
-  #m1name <- deparse(substitute(mtx1))
-  assign("contourValuesDisseminated", value = mtx1, envir = .GlobalEnv)
-  #assign("contourValuesComposite", value = mtx2, envir = .GlobalEnv)
-  #assign("contourValuesOverall", value = mtx3, envir = .GlobalEnv)
-  #contourValuesDisseminated <<- mtx1
-}
+
+
+dissContour <- filled.contour(x = coverageLevels, y = coverageLevels, z = as.matrix(contourValuesDisseminated))
+ggsave("DisseminatedContour.pdf", dissContour)
+overallContour <- filled.contour(x = coverageLevels, y = coverageLevels, z = as.matrix(contourValuesOverall))
+ggsave("OverallContour.pdf",overallContour)
+compositeContour <- filled.contour(x = coverageLevels, y = coverageLevels, z = as.matrix(contourValuesComposite))
+ggsave("CompositeContour.pdf", compositeContour)
